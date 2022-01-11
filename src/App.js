31dfa0bd08882f6f1d1f542376dd15c8c880fe61
Eunseo Jang <esjang@tl43.co.kr>
2022-01-11 14:43:38 +0900

    챔피언 초상화 리소스 추가, 소환사 숙련도 조회 기능 테스트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+const App = () => {
+  const [champList, setChampList] = useState([])
+  const [summonerName, setSummonerName] = useState('')
+
+  const handleInput = (e) => {
+    setSummonerName(e.target.value)
+  }
+
+  const handleClick = (e) => {
+    fetch(`http://localhost:3001/summoner/${summonerName}`)
+    .then(res => res.json())
+    .then(json => {
+      setChampList(json)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input value={summonerName} onChange={handleInput} />
+        <button onClick={handleClick}>소환사 조회</button>
+      </div>
+      <ul>
+        {
+          champList.map(champInfo => (
+            <li>
+              <img
+                src={`/images/portraits/120/${champInfo.key}.png`}
+                alt={champInfo.name}
+              />
+              {champInfo.name}
+            </li>
+          ))
+        }
+      </ul>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
